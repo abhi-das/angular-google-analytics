53836f3289ba4ff67dcb942441f840080edf209b
abhishekdass08@gmail.com <abhishekdass08@gmail.com>
2019-03-10 11:41:37 +0000

    update files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { Title } from '@angular/platform-browser';
+
+declare const ga: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'appga';
+  // title = 'appga';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HelpComponent } from './components/help/help.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { GoogleAnaEventTrackService } from './services/ga-event-tracking.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HelpComponent,
+    ContactUsComponent,
+    ProductListComponent,
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
-  providers: [],
+  providers: [ Title, GoogleAnaEventTrackService ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor(protected _googleAna: GoogleAnaEventTrackService) {}
+}
